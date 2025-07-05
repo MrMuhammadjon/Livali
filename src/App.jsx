7bce9cffffabdd16bd2db45aab1e6b0e986a1763
MrMuhammadjon <asliyevmukhammadjon@gmail.com>
2025-07-05 15:44:54 +0500

    remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AppContextProvider } from './Context/AppContext'
 import { Outlet } from 'react-router-dom'
-import Header from './сomponents/Header'
+import Header from './components/Header'
 import Footer from './components/Footer'
 
 const App = () => {
